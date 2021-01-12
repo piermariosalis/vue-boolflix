97047dac259b9c77d376961ab8bee9a4273c74b3
PierMario <piermario.salis@aol.dom>
2021-01-12 19:56:36 +0100

    Milestone 1 - Update 2
    Complete

---
+++ Milestone 1/assets/js/main.js
@@ -0,0 +1,26 @@
+let app = new Vue({
+  el: '#root',
+  data: {
+    movies: [
+
+    ],
+
+    keywords: '',
+
+  },
+  methods: {
+    getResponse_Results() {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=496c626a3913f2de74aae97a843da9bf', {
+          params: {
+            query: this.keywords,
+          }
+        })
+        .then(response => {
+          console.log(response);
+          this.movies = response.data.results;
+
+
+        })
+    }
+  },
+})
