866f129bacada1c2298d73ac57da00d31e592c67
PierMario <piermario.salis@aol.dom>
2021-01-14 13:14:08 +0100

    Milestone 2 - Update 2
    function forEach - Numeri interi (da 1 a 5) assegnati a icone

--- Milestone 2/assets/js/main.js
+++ Milestone 2/assets/js/main.js
@@ -6,9 +6,11 @@ let app = new Vue({
     ],
 
     keywords: '',
+     
 
   },
   methods: {
+   
     getResponse_Results() {
       axios.get('https://api.themoviedb.org/3/search/movie?api_key=496c626a3913f2de74aae97a843da9bf', {
           params: {
@@ -19,8 +21,16 @@ let app = new Vue({
           console.log(response);
           this.movies = response.data.results;
 
-
+          this.movies.forEach(starsRating => {
+            let toRate = parseInt(starsRating.vote_average/2);
+            starsRating.vote_average = toRate;
         })
+
+      })
+
     }
+
+    
   },
+  
 })
