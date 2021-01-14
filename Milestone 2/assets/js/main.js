f5bf5bdbec999f5342d6af678b095111fe4492af
PierMario <piermario.salis@aol.dom>
2021-01-14 21:10:42 +0100

    Milestone 2 - Update 3
    Ricerca serie TV con visualizzazione dei Dati e Flag della Lingua

--- Milestone 2/assets/js/main.js
+++ Milestone 2/assets/js/main.js
@@ -5,14 +5,23 @@ let app = new Vue({
 
     ],
 
-    keywords: '',
-     
+    series: [
 
+    ],
+
+    keywords: '',
+
+    keywordsTv: '',
+
+    flag: '',
   },
   methods: {
+
    
+
+
     getResponse_Results() {
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=496c626a3913f2de74aae97a843da9bf', {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=496c626a3913f2de74aae97a843da9bf&language=it-IT', {
           params: {
             query: this.keywords,
           }
@@ -21,16 +30,60 @@ let app = new Vue({
           console.log(response);
           this.movies = response.data.results;
 
+          
+
           this.movies.forEach(starsRating => {
             let toRate = parseInt(starsRating.vote_average/2);
             starsRating.vote_average = toRate;
-        })
 
+   
+            
+  
+        });
+
+      
+
+      });
+
+      axios.get('https://api.themoviedb.org/3/search/tv?api_key=496c626a3913f2de74aae97a843da9bf&language=en-EN', {
+        params: {
+          query: this.keywords,
+        }
+      })
+      .then(responseTv => {
+        this.series = responseTv.data.results;
+        console.log(responseTv);
+
+        this.series.forEach(starsRating => {
+          let toRate = parseInt(starsRating.vote_average/2);
+            starsRating.vote_average = toRate;
+        })
       })
 
-    }
 
+
+      
+
+    },
+
+    flagCountry(lang) {
+      lang.forEach(movies => {
+        this.flag = movies.original_language;
+        let countryFlag =
+          "https://www.countryflags.io/" + this.flag + "/flat/64.png";
+        return (movies.flag = countryFlag);
+      });
+    },
+    flagCountry(lang) {
+      lang.forEach(series => {
+        this.flag = series.original_language;
+        let countryFlag =
+          "https://www.countryflags.io/" + this.flag + "/flat/64.png";
+        return (series.flag = countryFlag);
+      });
+    },
     
-  },
+
+  }
   
 })
